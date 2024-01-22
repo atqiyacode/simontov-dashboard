cd8bc332880f77b78fdbc5ee3b47e516b71389a7
atqiyacode <atqiya@atqiyacode.com>
2024-01-23 06:55:21 +0700

    update static chart

--- src/views/MyNotificationPage.vue
+++ src/views/MyNotificationPage.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { ref } from 'vue';
-
+import { useMainStore } from '@/services/main.store';
+const mainStore = useMainStore();
 const tabs = ref([
     { title: 'Title 1', content: 'Content 1' },
     { title: 'Title 2', content: 'Content 2' },
@@ -9,60 +10,46 @@ const tabs = ref([
 </script>
 
 <template>
-    <div class="card">
-        <Accordion :activeIndex="0">
-            <AccordionTab v-for="tab in tabs" :key="tab.title" :header="tab.title" class="mb-3">
-                <p class="m-0">{{ tab.content }}</p>
-            </AccordionTab>
-        </Accordion>
-
-        <Panel
-            toggleable
-            v-for="(item, index) in 10"
-            :key="index"
-            class="mb-3 shadow-3"
-            :collapsed="true"
+    <ul class="list-none p-0 m-0">
+        <li
+            class="flex flex-column md:flex-row md:align-items-center md:justify-content-between mb-4"
         >
-            <template #header>
-                <div class="flex align-items-center gap-2">
-                    <span class="font-bold"> Notification title </span>
-                </div>
-            </template>
-            <template #footer>
-                <div class="flex flex-wrap align-items-center justify-content-between gap-3">
-                    <div class="flex align-items-center gap-2">
-                        <Button icon="pi pi-user" rounded text></Button>
-                        <Button icon="pi pi-bookmark" severity="secondary" rounded text></Button>
-                    </div>
-                    <span class="p-text-secondary">Updated 2 hours ago</span>
-                </div>
-            </template>
-            <template #icons>
-                <Menu ref="menu" id="config_menu" :model="items" popup />
-            </template>
+            <div>
+                <h5 class="text-gray-500 font-bold mr-2 mb-1 md:mb-0 capitalize">
+                    {{ mainStore.currentMap?.name }} - {{ mainStore.currentMap?.company_name }}
+                </h5>
+            </div>
+        </li>
+    </ul>
+    <Card
+        v-for="tab in tabs"
+        :key="tab.title"
+        :header="tab.title"
+        class="mb-3 shadow-3 bg-yellow-200"
+    >
+        <template #title>
+            <i class="pi pi-bell" style="font-size: 1.5rem"></i>
+            NOTIFICATION
+        </template>
+        <template #subtitle> Card subtitle </template>
+        <template #content>
             <p class="m-0">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute
-                irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                deserunt mollit anim id est laborum.
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur
+                error repudiandae numquam deserunt quisquam repellat libero asperiores earum nam
+                nobis, culpa ratione quam perferendis esse, cupiditate neque quas!
             </p>
-        </Panel>
+        </template>
+        <template #footer>
+            <Button icon="pi pi-check" label="Save" />
+            <Button
+                icon="pi pi-times"
+                label="Cancel"
+                severity="secondary"
+                style="margin-left: 0.5em"
+            />
+        </template>
+    </Card>
 
-        <Message
-            :closable="false"
-            severity="warn"
-            class="shadow-3"
-            v-for="(item, index) in 10"
-            :key="index"
-        >
-            <template #messageicon>
-                <Avatar image="/images/avatar/amyelsner.png" shape="circle" />
-            </template>
-            <span class="ml-2">How may I help you?</span>
-        </Message>
-    </div>
     <ul class="list-none p-0">
         <ListDetailBreak label="1." value="Data tidak masuk dalam 3 menit" />
         <ListDetailBreak label="2." value="Nilai totalizer tidak ada perubahan dalam 12 jam" />
